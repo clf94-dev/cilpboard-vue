5228347da98ae6e2b65d43503446531ac5b45a56
clf94-dev <clf94@hotmail.es>
2020-11-01 14:58:03 +0100

    added companies html struct

--- src/components/Companies.vue
+++ src/components/Companies.vue
@@ -1,6 +1,24 @@
 <template>
 <div>
-    <h1>companies component</h1>
+    <b-container fluid class="companies-section">
+        <b-row>
+            <b-col class="img-col" xs="12" sm="6" md="2" lg="2">
+                <img :src="google" alt="Google">
+            </b-col>
+            <b-col class="img-col" xs="12" sm="6" md="2" lg="2">
+                <img :src="hp" alt="HP">
+            </b-col>
+            <b-col class="img-col" xs="12" sm="6" md="2" lg="2">
+                <img :src="ibm" alt="IBM">
+            </b-col>
+            <b-col class="img-col" xs="12" sm="6" md="2" lg="2">
+                <img :src="microsoft" alt="Microsoft">
+            </b-col>
+            <b-col class="img-col" xs="12" sm="6" md="2" lg="2">
+                <img :src="vector" alt="Vector">
+            </b-col>
+        </b-row>
+    </b-container>
 </div>
 </template>
 
@@ -9,7 +27,11 @@ export default {
     name: 'Companies',
     data() {
         return {
-            desktopImg: require("@/assets/images/image-computer.png")
+            google: require("@/assets/images/logo-google.png"),
+            hp: require("@/assets/images/logo-hp.png"),
+            microsoft: require("@/assets/images/logo-microsoft.png"),
+            ibm: require("@/assets/images/logo-ibm.png"),
+            vector: require("@/assets/images/logo-vector-graphics.png"),
         }
     }
 }
