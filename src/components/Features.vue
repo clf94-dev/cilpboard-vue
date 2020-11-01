391eb21738aeae66f1a184e5d48628b6cf1b9dae
clf94-dev <clf94@hotmail.es>
2020-11-01 18:48:14 +0100

    added aos animation features component

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,13 +1,15 @@
 <template>
 <b-container fluid class="features-section">
-    <h2>Supercharge your workflow</h2>
-    <p>We’ve got the tools to boost your productivity.</p>
+    <h2 data-aos="fade-right" data-aos-delay="100">Supercharge your workflow</h2>
+    <p data-aos="fade-right" data-aos-delay="100">We’ve got the tools to boost your productivity.</p>
     <b-row class="row-feat">
         <b-col xs="12" sm="4" md="4" lg="4" v-for="feat of features" v-bind:key="feat.title">
-            <img :src="feat.src" :alt="feat.imgAlt">
+            <b-col data-aos="fade-up" v-bind:data-aos-delay="feat.delay">
+                <img :src="feat.src" :alt="feat.imgAlt">
 
-            <h4>{{feat.title}}</h4>
-            <p>{{feat.text}}</p>
+                <h4>{{feat.title}}</h4>
+                <p>{{feat.text}}</p>
+            </b-col>
         </b-col>
 
     </b-row>
@@ -23,19 +25,22 @@ export default {
                     title: 'Create blacklists',
                     text: 'Ensure sensitive information never makes its way to your clipboard by excluding certain sources.',
                     src: require("@/assets/images/icon-blacklist.svg"),
-                    imgAlt: 'Create blacklists'
+                    imgAlt: 'Create blacklists',
+                    delay: 100
                 },
                 {
                     title: 'Plain text snippets',
                     text: 'Remove unwanted formatting from copied text for a consistent look.',
                     src: require("@/assets/images/icon-text.svg"),
-                    imgAlt: 'Plain text snippets'
+                    imgAlt: 'Plain text snippets',
+                    delay: 300
                 },
                 {
                     title: 'Sneak preview',
                     text: 'Quick preview of all snippets on your Clipboard for easy access.',
                     src: require("@/assets/images/icon-preview.svg"),
-                    imgAlt: 'Sneak preview'
+                    imgAlt: 'Sneak preview',
+                    delay: 500
                 }
 
             ]
@@ -52,6 +57,10 @@ export default {
 
 .features-section h2 {
     font-size: 30px;
+}
+
+img {
+    margin-bottom: 20px;
 }
 
 .features-section p {
