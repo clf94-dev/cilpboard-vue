0eb614d506de1f312a571dda083708ed6375e2da
clf94-dev <clf94@hotmail.es>
2020-11-01 18:23:20 +0100

    added aos config

--- src/main.js
+++ src/main.js
@@ -7,10 +7,15 @@ import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  created() {
+    AOS.init();
+  },
 }).$mount('#app')
